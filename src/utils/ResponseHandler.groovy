4a20a45f709b0f6f97aa99c91ba46735b8fa9aa5
kevdaci <kevdaci@gmail.com>
2024-06-19 16:47:51 -0400

    Add logs for response

--- src/utils/ResponseHandler.groovy
+++ src/utils/ResponseHandler.groovy
@@ -16,6 +16,7 @@ class ResponseHandler {
 	static void printJsonResponse(HttpServletResponse httpServletResponse, response) {
 		setContentTypeToApplicationJson(httpServletResponse);
 		def jsonResponse = toJsonResponse(response);
+		println("Response: " + jsonResponse);
 		PrintWriter out = httpServletResponse.getWriter();
 		out.println(jsonResponse);
 	}
